df62d042894ee17c151a9ded119cff26c454928f
surbhitt <surbhit.thakur@gmail.com>
2024-02-10 20:33:40 +0530

    refactor: add searchParams to pass as props to children

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import Bar from "./Bar";
-import WeatherCard from "./WeatherCard";
+// import WeatherCard from "./WeatherCard";
+import CityGrid from "./container-comp/Grid";
+import { useState } from "react";
 
 export default function Container() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  // const [loc] = useState(searchParams.get("loc") ?? "");
+  const [unit] = useState(searchParams.get("unit") ?? "");
+
   return (
     <div className="min-w-[300px] w-5/6 lg:w-2/3 grid gap-3">
-      <Bar />
-      <div className="p-2 w-full border border-gray-500 rounded border-solid">
-        <WeatherCard />
+      <Bar searchParams={searchParams} setSearchParams={setSearchParams} />
+      <div className="w-full">
+        <CityGrid unit={unit} />
+        {/* <WeatherCard /> */}
       </div>
     </div>
   );
