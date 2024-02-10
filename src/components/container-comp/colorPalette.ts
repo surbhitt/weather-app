354c92db356685dd010abd5e21570acd77a7d516
surbhitt <surbhit.thakur@gmail.com>
2024-02-10 20:32:37 +0530

    chore: add color palette and values

---
+++ src/components/container-comp/colorPalette.ts
@@ -0,0 +1,38 @@
+const palette = {
+  gb_bright_red: "bg-[#fb4934]", // 167]     " 251-73-52
+  gb_bright_orange: "bg-[#fe8019]", // 208]     " 254-128-25
+  gb_bright_yellow: "bg-[#fabd2f]", // 214]     " 250-189-47
+  gb_bright_green: "bg-[#b8bb26]", // 142]     " 184-187-38
+  gb_bright_aqua: "bg-[#8ec07c]", // 108]     " 142-192-124
+  gb_bright_blue: "bg-[#83a598]", // 109]     " 131-165-152
+  // gb_bright_purple: "bg-[#d3869b]", // 175]     " 211-134-155
+};
+
+// const alt_palette = {
+//   gb_neutral_red: "bg-[#cc241d]", // 124]     " 204-36-29
+//   gb_neutral_orange: "bg-[#d65d0e]", // 166]     " 214-93-14
+//   gb_neutral_yellow: "bg-[#d79921]", // 172]     " 215-153-33
+//   gb_neutral_green: "bg-[#98971a]", // 106]     " 152-151-26
+//   gb_neutral_aqua: "bg-[#689d6a]", // 72]      " 104-157-106
+//   gb_neutral_blue: "bg-[#458588]", // 66]      " 69-133-136
+//   gb_neutral_purple: "bg-[#b16286]", // 132]     " 177-98-134
+//   gb_faded_red: "bg-[#9d0006]", // 88]      " 157-0-6
+//   gb_faded_orange: "bg-[#af3a03]", // 130]     " 175-58-3
+//   gb_faded_yellow: "bg-[#b57614]", // 136]     " 181-118-20
+//   gb_faded_green: "bg-[#79740e]", // 100]     " 121-116-14
+//   gb_faded_purple: "bg-[#8f3f71]", // 96]      " 143-63-113
+//   gb_faded_aqua: "bg-[#427b58]", // 66]      " 66-123-88
+//   gb_faded_blue: "bg-[#076678]", // 24]      " 7-102-120
+// };
+
+// const Colors = [
+//   "bg-[#8BE9FD]", //Color_Dracula_Cyan
+//   "bg-[#6272A4]", //Color_Dracula_Comment
+//   "bg-[#50FA7B]", //Color_Dracula_Green
+//   "bg-[#FFB86C]", //Color_Dracula_Orange
+//   "bg-[#FF79C6]", //Color_Dracula_Pink
+//   "bg-[#BD93F9]", //Color_Dracula_Purple
+//   "bg-[#FF5555]", //Color_Dracula_Red
+// ];
+
+export default palette;
