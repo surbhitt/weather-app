10882d260e48825932010c19da0235099ac3d37d
surbhitt <surbhit.thakur@gmail.com>
2024-02-11 07:59:42 +0530

    feat: add suggestion for past searches

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export default function Header() {
   const [loc, setLoc] = useState("");
   const [history, setHistory] = useState<string[]>([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [show, setShow] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(event.target as Node)
+      ) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const addToHistory = () => {
-    if (history.length > 5) history.length = 5;
+    if (history.length > 4) history.length = 4;
     setHistory([loc, ...history]);
   };
 
@@ -25,11 +43,26 @@ export default function Header() {
       <img src="/logo-alt.svg" height={40} width={40} />
       <div className="w-2/3 lg:w-1/3 min-w-[150px] relative flex">
         <input
-          className="w-full pl-5 pr-14 border rounded-full text-[0.9rem] placeholder:text-gray-500 border-gray-400 h-10  outline-none"
+          onFocus={() => setShow(true)}
+          ref={inputRef}
+          className="relative w-full pl-5 pr-14 border rounded-full text-[0.9rem] placeholder:text-gray-500 border-gray-400 h-10  outline-none"
           value={loc}
           placeholder="Search location..."
           onChange={(e) => setLoc(e.target.value)}
         />
+        {show && history.length !== 0 && (
+          <div className="absolute rounded-xl top-12 w-full border border-solid bg-white z-20 border-gray-400">
+            {history.map((sug, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="p-3 my-2 hover:bg-blue-100 transition cursor-pointer">
+                  {sug}
+                </div>
+              );
+            })}
+          </div>
+        )}
         <div
           className="h-9 w-12 flex gap-3 rounded-r-full hover:bg-gray-100  absolute right-0.5 top-0.5 cursor-pointer"
           onClick={handleSearch}>
