65c9ae2649b6419846211f2cf67a8d1579b659b2
surbhitt <surbhit.thakur@gmail.com>
2024-02-11 07:59:07 +0530

    refactor: add weathercard to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,20 +1,20 @@
 import { useSearchParams } from "react-router-dom";
 import Bar from "./Bar";
-// import WeatherCard from "./WeatherCard";
+import WeatherCard from "./WeatherCard";
 import CityGrid from "./container-comp/Grid";
 import { useState } from "react";
 
 export default function Container() {
   const [searchParams, setSearchParams] = useSearchParams();
-  // const [loc] = useState(searchParams.get("loc") ?? "");
+  const [loc] = useState(searchParams.get("loc") ?? "");
   const [unit] = useState(searchParams.get("unit") ?? "");
+  console.log("loc", loc);
 
   return (
     <div className="min-w-[300px] w-5/6 lg:w-2/3 grid gap-3">
       <Bar searchParams={searchParams} setSearchParams={setSearchParams} />
       <div className="w-full">
-        <CityGrid unit={unit} />
-        {/* <WeatherCard /> */}
+        {loc ? <WeatherCard unit={unit} /> : <CityGrid unit={unit} />}
       </div>
     </div>
   );
