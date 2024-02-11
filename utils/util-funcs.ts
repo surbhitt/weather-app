56c6f760d3fd44fa735afbc2acd5e9b76524f7cd
surbhitt <surbhit.thakur@gmail.com>
2024-02-11 07:57:04 +0530

    refactor: move calculateTemp & accent to a seperate file

---
+++ utils/util-funcs.ts
@@ -0,0 +1,18 @@
+export const calculateTemp = (temp: number, unit: string) => {
+  const tempC = temp - 273;
+  let tempFinal;
+  if (unit === "far") {
+    tempFinal = `${tempC * (9 / 5) + 32} °F`;
+  } else tempFinal = `${tempC.toFixed(1)} °C`;
+  return tempFinal;
+};
+
+export const calculateAccent = (temp: number) => {
+  const tempC = temp - 273;
+  if (tempC < 10) return "bg-[#83a598]";
+  else if (10 <= tempC && tempC < 20) return "bg-[#8ec07c]";
+  else if (20 <= tempC && tempC < 30) return "bg-[#b8bb26]";
+  else if (30 <= tempC && tempC < 40) return "bg-[#fabd2f]";
+  else if (40 <= tempC && tempC < 50) return "bg-[#fe8019]";
+  else return "bg-[#fb4934]";
+};
