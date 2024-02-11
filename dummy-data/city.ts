d3a494d654f8ed6b904e62b7e25f714de183283d
surbhitt <surbhit.thakur@gmail.com>
2024-02-11 07:58:07 +0530

    chore: add dummy data

---
+++ dummy-data/city.ts
@@ -0,0 +1,7 @@
+export const cities = [
+  { name: "Amsterdam", country: "NE", maxT: "10", minT: "2" },
+  { name: "Athens", country: "GR", maxT: "30", minT: "12" },
+  { name: "Barcelona", country: "SP", maxT: "15", minT: "8" },
+  { name: "Beijing", country: "NE", maxT: "12", minT: "-2" },
+  { name: "Berlin", country: "GE", maxT: "20", minT: "5" },
+];
